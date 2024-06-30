d5d1a8031be2c7d3c2f829f682207397363ddf20
Awadesh <36awadesh@gmail.com>
2024-06-30 18:08:45 +0530

    Final commit and Debugging

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Card() {
-  let avatarUrl = "https://avatars.githubusercontent.com/u/76896819?v=4";
-
+  let avatarUrl = "https://avatars.githubusercontent.com/u/76896819?v=";
   return (
     <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <a href="/">
@@ -15,7 +15,7 @@ export default function Card() {
       <div className="px-5 pb-5">
         <a href="/">
           <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+            Hi, this is Awadesh, Thanks for visiting.
           </h5>
         </a>
         <div className="flex items-center mt-2.5 mb-5">
@@ -56,7 +56,7 @@ export default function Card() {
             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
           </svg>
           <svg
-            className="w-4 h-4 text-gray-200 dark:text-gray-600"
+            className="w-4 h-4 text-yellow-300 mr-1"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
@@ -64,19 +64,17 @@ export default function Card() {
           >
             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
           </svg>
-          <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
-            4.0
+          <span className="bg-yellow-300 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+            5.0
           </span>
         </div>
         <div className="flex items-center justify-between">
-          <span className="text-3xl font-bold text-gray-900 dark:text-white">
-            $599
-          </span>
+          <span className="text-3xl font-bold text-gray-900 dark:text-white"></span>
           <a
-            href="/"
+            href="https://github.com/Awadesh365"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Add to cart
+            Check out Github
           </a>
         </div>
       </div>
